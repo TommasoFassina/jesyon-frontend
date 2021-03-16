b1548d429d7165a91922317c2463dba19f4226c5
TommasoFassina <tommy.fass@gmail.com>
2021-03-16 13:20:00 +0100

    current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
        trigger: 'hover'
     });
  });
+
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("datee").innerHTML = d + "/" + m + "/" + y;
+console.log(d + "/" + m + "/" + y);
