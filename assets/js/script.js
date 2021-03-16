3cbeba44167aa7c6ca9d21ce1302e164423b6816
TommasoFassina <tommy.fass@gmail.com>
2021-03-16 20:52:55 +0100

    data function fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,13 @@ $(document).ready(function() {
     
  });
 
+function data(){
 n =  new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("datee").innerHTML = d + "/" + m + "/" + y;
 console.log(d + "/" + m + "/" + y);
+}
+
 
